12c648f8b672e3eaf70c7fcd148cfa8d05f2f516
Andre Chaves <andrechavesg@gmail.com>
2018-05-08 21:56:14 -0300

    capitulo3

--- projeto/gulpfile.js
+++ projeto/gulpfile.js
@@ -1,7 +1,15 @@
 var gulp = require('gulp')
   ,imagemin = require('gulp-imagemin')
-  ,clean = require('gulp-clean');
+  ,clean = require('gulp-clean')
+  ,concat = require('gulp-concat')
+  ,htmlReplace = require('gulp-html-replace')
+  ,uglify = require('gulp-uglify')
+  ,usemin = require('gulp-usemin')
+  ,cssmin = require('gulp-cssmin');
 
+gulp.task('default', ['copy'], function() {
+    gulp.start('build-img', 'usemin');
+});
 
 gulp.task('copy', ['clean'], function() {
     return gulp.src('src/**/*')
@@ -13,9 +21,18 @@ gulp.task('clean', function() {
         .pipe(clean());
 });
 
-gulp.task('build-img', ['copy'], function() {
+gulp.task('build-img', function() {
 
-  gulp.src('dist/img/**/*')
+  return gulp.src('dist/img/**/*')
     .pipe(imagemin())
     .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('usemin', function() {
+  return gulp.src('dist/**/*.html')
+    .pipe(usemin({
+      js: [uglify()],
+      css: [cssmin()]
+    }))
+    .pipe(gulp.dest('dist'));
 });
